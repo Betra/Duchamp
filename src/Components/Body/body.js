61cfc0f9a18c8c785fc258b45b07f24889b1a6a2
Ivan Dorofeyev <IDDorofeev@yahoo.com>
2019-02-16 16:29:34 +0300

    Component Init

---
+++ src/Components/Body/body.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import html from './body.css';
+
+function Body(props) {
+  return (
+      <div>
+        Hello World!
+      </div>
+    );
+}
+
+export default Body
