7ffaaeb12898bce0c037bd5c2fb9a6e0d9d3900f
Ivan Dorofeyev <IDDorofeev@yahoo.com>
2019-02-16 17:13:13 +0300

    Text component imported

--- src/Components/Body/body.js
+++ src/Components/Body/body.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import html from './body.css';
+import Text from '../Text/Text';
 
 function Body(props) {
   return (
       <div>
         Hello World!
+      <Text />
       </div>
     );
 }
+
+// <Header /> <Wrapper />< Footer />
 
 export default Body
