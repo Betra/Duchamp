53eb409bd5f9230f811825a9385033985e05f0b1
Ivan Dorofeyev <IDDorofeev@yahoo.com>
2019-02-16 17:13:03 +0300

    Text state added

--- src/Components/Text/Text.js
+++ src/Components/Text/Text.js
@@ -2,9 +2,33 @@ import React, { Component } from 'react';
 import styles from './Text.css';
 
 class Text extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {text: ''};
+  }
+
+  handleInput = event => this.setState({text: event.target.value});
+  
+  handleSubmit(event) {
+      alert(this.state.text);
+      event.preventDefault();
+  }
+
   render() {
     return (
-      <textarea className={styles.inputArea} />
+      <form onSubmit={this.handleSubmit}>
+      <textarea 
+        className={styles.inputArea} 
+        value={this.state.text} 
+        onChange={this.handleInput}
+        placeholder='Insert your text here' 
+        />
+        <br />
+        <input type ="submit" />
+      </form>
     );
   }
 }
