5edf8116749179e76a3c67fc708b36b14321b3e6
Ivan Dorofeyev <IDDorofeev@yahoo.com>
2019-02-18 01:07:15 +0300

    New Logic Acquired

---
+++ src/Components/Popup/WelcomePop/WelcomePop.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styles from './WelcomePop.module.css';
+
+function WelcomePop(props) {
+  return(
+    <section className={styles.WelcomeSection}>
+      asdasd
+    </section>
+  );
+}
+
+export default WelcomePop;
