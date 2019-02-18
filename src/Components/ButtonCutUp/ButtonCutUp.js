39bde4c8d0610e540ef0d6663dc20e65ea18c8ba
Ivan Dorofeyev <IDDorofeev@yahoo.com>
2019-02-19 00:39:30 +0300

    Cut Up Algorithm

--- src/Components/ButtonCutUp/ButtonCutUp.js
+++ src/Components/ButtonCutUp/ButtonCutUp.js
@@ -3,18 +3,23 @@ import styles from './buttonCutUp.module.css';
 import Button from '../Button/Button';
 
 function ButtonCutUp(props) {
-  const text = 'Hello World, My Name Is Igor. Im from Russia';
+  const text = 'Hello World, My Name Is Igor. Im from Russia! Do you know it?';
   const WordsAmount = text.split(" ").length;
 
   //Cut up by sentences:
-const sentences = text.match(/(.*?[.?!])(?: |$)/g)
+  const sentences = text.match( /[^\.!\?]+[\.!\?]+/g ); 
+  // I want to take sentences from text
+  // take random(1, sentence.length) amount of words in it 
+  // These words are compiled as a "Cut Up" and excluded from this sentence
+  // Nice solution?
 
   if (WordsAmount > 3) {
-    //Generate Random amount of word in a current sentence in a cut, from one to full sentence
+    //Generate Random amount of word in a current sentence in a cut, from one to full sentences
   }
-
-  return (<h1>{sentences.length}</h1>);
+  return(
+    <Button
+    />
+  );
 }
-ButtonCutUp()
 
 export default ButtonCutUp;
