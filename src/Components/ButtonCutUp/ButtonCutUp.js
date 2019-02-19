9b80014e8053b62ef435246c75c557a71a505676
Ivan Dorofeyev <IDDorofeev@yahoo.com>
2019-02-19 21:59:53 +0300

    camelCase adjustments, named export

--- src/Components/ButtonCutUp/ButtonCutUp.js
+++ src/Components/ButtonCutUp/ButtonCutUp.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import styles from './buttonCutUp.module.css';
-import Button from '../Button/Button';
-import { getRandomInRange } from '../../utils/getRandomInRange';
 
-function ButtonCutUp(props) {
+import { Button } from '../Button';
+import { getRandomInRange } from '../../utils';
+
+export function ButtonCutUp(props) {
   const text = 'Hello World, My Name Is Igor. Im from Russia! Do you know it?'; //this.value.text in future
-  const WordsAmount = text.split(/ /).length; //Rough amount of words in the text
   const sentences = text.match( /[^\.!\?]+[\.!\?]+/g ); // returns array with sentences
-  const CutUps = []; 
+  const cutUps = []; 
 
   while (sentences.length > 0) { // separate file?
     let sentence = sentences[0].split(/ /); // array with words of this sentences
@@ -16,8 +16,8 @@ function ButtonCutUp(props) {
       let cutAt = getRandomInRange(1,sentence.length +1); // returns random amount of words in this sentence
       
       if (cutAt!=(sentence.length)) { 
-        const CutUp = sentence.splice(0,cutAt);
-        CutUps.push(CutUp.join(' ')); // add the cutup to the array
+        const cutUp = sentence.splice(0,cutAt);
+        cutUps.push(cutUp.join(' ')); // add the cutup to the array
         
        if (sentence.length <= 0) {
          sentence.shift();
@@ -26,7 +26,7 @@ function ButtonCutUp(props) {
         }
 
       } else {
-        CutUps.push(sentence.join(" "));
+        cutUps.push(sentence.join(" "));
         sentences.shift();
         break;
       }
@@ -38,5 +38,3 @@ function ButtonCutUp(props) {
     />
   );
 }
-
-export default ButtonCutUp;
