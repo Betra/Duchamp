c387fa770d0afb3052dbe6904755119199784e6d
Ivan Dorofeyev <IDDorofeev@yahoo.com>
2019-02-22 04:36:10 +0300

    Sentences generator included, bug found

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import styles from './button.module.css';
+import React from 'react';
+//import styles from './button.module.css';
 
 //
 // <Button click={} text={}/>
--- src/Components/ButtonCutUp/cutRandomPieces.js
+++ src/Components/ButtonCutUp/cutRandomPieces.js
@@ -1,23 +1,27 @@
 import { getRandomInRange } from '../../utils';
 
-export function cutRandomPieces(sentences) {
+export function cutRandomPieces(text) {
+  // Bug with 0 sentences
+  let sentences = text.match( /[^\.!\?]+[\.!\?]+/g );
+
   const cutUps = []; 
 
-  while (sentences.length > 0) {
-    let sentence = sentences[0].split(/ /); // array with words of this sentences
-    
-    while (true) {
-      let wordsInCutUp = getRandomInRange(1,sentence.length+1); // returns random amount of words in this sentence
-      let cutUp = sentence.splice(0,wordsInCutUp);
-  
-
-      cutUp = cutUp.join(' ');
-      cutUps.push(cutUp); // add the cutup to the array
+    while (sentences.length > 0) {
+      let sentence = sentences[0].split(/ /); // array with words of this sentences
+      
+      while (true) {
+        let wordsInCutUp = getRandomInRange(1,sentence.length+1); // returns random amount of words in this sentence
+        let cutUp = sentence.splice(0,wordsInCutUp);
     
 
-      if (sentence.length === 0) { sentences.shift(); break;}
+        cutUp = cutUp.join(' ');
+        cutUps.push(cutUp); // add the cutup to the array
+      
+
+        if (sentence.length === 0) { sentences.shift(); break;}
+      }
     }
-  }
+
 
   return cutUps.filter(Boolean);
 }
