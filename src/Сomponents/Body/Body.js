0cada7093d5bc6dead9719d3cfdc6af9d96bae19
Ivan Dorofeyev <IDDorofeev@yahoo.com>
2019-02-23 20:52:59 +0300

    Output wrapped in paper

--- src/Сomponents/Body/Body.js
+++ src/Сomponents/Body/Body.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styles from './Body.module.css';
 import globalStyles from '../../CSS/global.css';
 import { Form } from '../Form';
+import { Paper} from '../Paper';
 
 export class Body extends React.Component {
   constructor(props) {
@@ -13,10 +14,11 @@ export class Body extends React.Component {
   handleCutUpArray = cutUpArray => this.setState({cutUps: cutUpArray});
 
   render() {
+    const { cutUps } = this.state;
     return(
       <main>
         <Form onSubmit={this.handleCutUpArray}/>
-        {this.state.cutUps}
+        {cutUps.map((cutUp)=> <Paper>{cutUp}</Paper>)}
       </main>
     );
   }
