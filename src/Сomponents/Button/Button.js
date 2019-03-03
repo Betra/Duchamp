4feb66f1405b556c2f85db2f13958db895eb0738
Ivan Dorofeyev <IDDorofeev@yahoo.com>
2019-03-04 02:37:26 +0300

    build fix

--- src/Сomponents/Button/Button.js
+++ src/Сomponents/Button/Button.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import styles from './Button.module.css';
+import styles from './button.module.css';
 
 export const Button = ({onClick, disabled, text, value}) => (
   <button onClick={onClick}
