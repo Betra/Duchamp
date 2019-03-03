6a097957c64b68596fce3ad3bbef2a1f46f9185c
Ivan Dorofeyev <IDDorofeev@yahoo.com>
2019-03-04 01:05:20 +0300

    textarea height fixated

--- src/Сomponents/Text/Text.js
+++ src/Сomponents/Text/Text.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import styles from './text.module.css';
 import { getRandomPlaceholder} from '../../utils';
 
@@ -8,22 +9,23 @@ export class Text extends React.Component {
   constructor(props) {
     super(props);
     this.handleInput = this.handleInput.bind(this);
-    this.state = { rows: 1 };
+    this.state = { rows: 6 };
   }
 
   handleInput(event) { 
-    const oldRows = event.target.rows;
-    event.target.rows = 1;
-    const newRows = ~~(event.target.scrollHeight/lineHeight);
+    // const oldRows = event.target.rows;
+    // event.target.rows = 1;
+    // const newRows = ~~(event.target.scrollHeight/lineHeight);
 
-    if (newRows === oldRows) { event.target.rows = newRows; }
-    this.setState({rows: newRows});
+    // if (newRows === oldRows) { event.target.rows = newRows; }
+    // this.setState({rows: newRows});
 
     this.props.onTextChange(event.target.value);
   }
 
   render() {
     const { text } = this.props;
+
     return (
           <textarea autoFocus
             rows={this.state.rows}
