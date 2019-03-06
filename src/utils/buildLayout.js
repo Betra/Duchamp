ca1ac1d5a4538b143a0a436afea9b0abd3b1d9d6
Ivan Dorofeyev <IDDorofeev@yahoo.com>
2019-03-07 02:50:10 +0300

    camelCase fix

--- src/utils/buildLayout.js
+++ src/utils/buildLayout.js
@@ -1,17 +1,17 @@
 export function buildLayout(parent,cols) {
-  var COL_COUNT = cols; 
-  var col_heights = []
+  var columnCount = cols; 
+  var columnHeight = []
   var container = document.getElementById(parent);
 
-  for (var i = 0; i <= COL_COUNT; i++) {
-    col_heights.push(0);
+  for (var i = 0; i <= columnCount; i++) {
+    columnHeight.push(0);
   }
 
   for (var j = 0; j < container.children.length; j++) {
-    var order = (j + 1) % COL_COUNT || COL_COUNT;
+    var order = (j + 1) % columnCount || columnCount;
     container.children[j].style.order = order;
-    col_heights[order] += parseFloat(container.children[j].style.height);
+    columnHeight[order] += parseFloat(container.children[j].style.height);
   }
-  // var highest = Math.max.apply(Math, col_heights);
+  // var highest = Math.max.apply(Math, columnHeight);
   // container.style.height = highest+'px';
 }
