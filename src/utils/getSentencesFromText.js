52213e795b9a5f9c9a36410bcdffa6fc5d59de43
Ivan D <DivanDoesCode@gmail.com>
2019-03-10 02:02:12 +0300

    regexp Fix

--- src/utils/getSentencesFromText.js
+++ src/utils/getSentencesFromText.js
@@ -1,10 +1,12 @@
 export const getSentencesFromText = (text, amount = 0) => {
-  if (!text) return "";
+  if (!text || typeof text !== "string") return null;
 
   text.match(/[!.?]$/) ? (text = String(text)) : (text += ".");
   text = text.replace(/[\r\n]+/, "");
 
   const sentences = text.match(/[^.!?]+[.!?]+/g);
+
+  if (!sentences) return [];
 
   if (amount !== 0) {
     let sentencesText = "";
