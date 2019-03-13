36b85b6492cbbd0858b1532629d1cf51e1dffc81
Ivan D <DivanDoesCode@gmail.com>
2019-03-13 19:04:30 +0300

    Fonts added

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 
 import "./CSS/global.css";
-import { Body } from "./Сomponents/Body";
+import { App } from "./Сomponents/App";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
-    <Body />
+    <App />
   </BrowserRouter>,
   document.getElementById("root")
 );
