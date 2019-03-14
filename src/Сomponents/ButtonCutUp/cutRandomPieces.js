4043a4d18d4adcb472e69515bfc4a77df1a8f909
Ivan D <DivanDoesCode@gmail.com>
2019-03-14 22:24:08 +0300

    now 4 words per  paper max

--- src/Сomponents/ButtonCutUp/cutRandomPieces.js
+++ src/Сomponents/ButtonCutUp/cutRandomPieces.js
@@ -14,7 +14,15 @@ export function cutRandomPieces(text) {
     let sentence = sentences[0].split(/ /); // array with words of this sentences
 
     while (true) {
-      let wordsInCutUp = getRandomInRange(1, sentence.length + 1); // returns random amount of words in this sentence
+      let wordsInCutUp; // returns random amount of words in this sentence
+
+      while (true) {
+        wordsInCutUp = getRandomInRange(1, sentence.length + 1);
+
+        if (wordsInCutUp > 4) continue;
+        else break;
+      }
+
       let cutUp = sentence.splice(0, wordsInCutUp);
 
       cutUp = cutUp.join(" ");
