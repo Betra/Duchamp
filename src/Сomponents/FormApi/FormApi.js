619119eb105952972f97cdd9faed5670720a1b96
Ivan D <DivanDoesCode@gmail.com>
2019-03-18 22:37:55 +0300

    Now functioning

--- src/Сomponents/FormApi/FormApi.js
+++ src/Сomponents/FormApi/FormApi.js
@@ -2,10 +2,34 @@ import React from "react";
 
 import { ApiInfo } from "../ApiInfo";
 import { Button } from "../Button";
+import { LoadingApi } from "../LoadingApi";
+import { ErrorMessage } from "../ErrorMessage";
+import { Cutups } from "../Cutups";
 
-export const FormApi = ({ link, title, brief, image }) => (
-  <section>
-    <ApiInfo link={link} title={title} brief={brief} imageUrl={image} />
-    <Button onClick={onsubmit} />
-  </section>
-);
+export const FormApi = ({
+  link,
+  title,
+  brief,
+  image,
+  onSubmit,
+  hasError,
+  error,
+  isLoading,
+  onNext,
+  cutups
+}) => {
+  if (isLoading) return <LoadingApi />;
+  else if (hasError) return <ErrorMessage error={error} />;
+  else {
+    return (
+      <main>
+        <section>
+          <ApiInfo link={link} title={title} brief={brief} imageUrl={image} />
+          <Button onClick={onSubmit} />
+          <Button onClick={onNext} value="Next" />
+        </section>
+        <Cutups cutups={cutups} />
+      </main>
+    );
+  }
+};
